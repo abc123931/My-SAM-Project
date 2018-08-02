4b80bf90270674450cad08deae5a259a10b2e15b
abc123931 <abc123931@gmail.com>
2018-08-03 08:36:27 +0900

    lambdaを2つに追加

--- hello-world/main_test.go
+++ hello-world/main_test.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"testing"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestHandler(t *testing.T) {
@@ -11,16 +12,12 @@ func TestHandler(t *testing.T) {
 	request := events.APIGatewayProxyRequest{}
 	expectedResponse := events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Headers: map[string]string{
-			"Content-Type": "text/html",
-		},
-		Body: "Congratulations",
+		Body:       "Hello World",
 	}
 
 	response, err := Handler(request)
 
-	assert.Equal(t, response.Headers, expectedResponse.Headers)
-	assert.Contains(t, response.Body, expectedResponse.Body)
+	assert.Equal(t, response.Body, expectedResponse.Body)
 	assert.Equal(t, err, nil)
 
 }
